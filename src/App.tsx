8abf38daffe6ceccf4c06e8954dd2fa0730fb798
BarryDong <286084845@qq.com>
2023-09-02 21:32:24 +0800

    完成 ProductCollection 组件基本样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Typography } from 'antd';
+
 import styles from './App.module.css';
-import { TopHeader, Header, Footer, SideMenu, Carousel } from './components';
+import { TopHeader, Header, Footer, SideMenu, Carousel, ProductCollection } from './components';
+import { productList1, productList2, productList3 } from './mockup';
+import sideImage from './assets/images/sider_2019_12-09.png';
+import sideImage2 from './assets/images/sider_2019_02-04.png';
+import sideImage3 from './assets/images/sider_2019_02-04-2.png';
 
 function App() {
   return (
@@ -9,7 +14,8 @@ function App() {
         <TopHeader />
         <Header />
 
-        <div className={styles["page-content"]}>
+        <div className={styles['page-content']}>
+
           <Row style={{ marginTop: 20 }}>
             <Col span={6} style={{ height: 200 }}>
               <SideMenu />
@@ -18,6 +24,23 @@ function App() {
               <Carousel />
             </Col>
           </Row>
+
+          <ProductCollection
+              title={<Typography.Title level={3} type="warning">爆款推荐</Typography.Title>}
+              sideImage={sideImage}
+              products={productList1}
+          />
+          <ProductCollection
+              title={<Typography.Title level={3} type="danger">新品上市</Typography.Title>}
+              sideImage={sideImage2}
+              products={productList2}
+          />
+          <ProductCollection
+              title={<Typography.Title level={3} type="success">国内游推荐</Typography.Title>}
+              sideImage={sideImage3}
+              products={productList3}
+          />
+
         </div>
 
         <Footer />
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import SideMenu from './SideMenu/SideMenu';
 import Carousel from './Carousel/Carousel';
+import ProductCollection from './ProductCollection/ProductCollection';
 
-export { TopHeader, Header, Footer, SideMenu, Carousel };
+export { TopHeader, Header, Footer, SideMenu, Carousel, ProductCollection };
